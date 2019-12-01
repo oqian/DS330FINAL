8490ac9ce03aef7ccf791ba29307bcf98ca9b329
Jiarong Ye <33590964+karenyyy@users.noreply.github.com>
2019-12-01 02:09:03 -0500

    Update preprocessing.py

--- py_files/preprocessing.py
+++ py_files/preprocessing.py
@@ -9,7 +9,7 @@ GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIz
 
 
 def clean_review(raw_file_path, save_file_path):
-    df = pd.read_csv('/home/karen/workspace/ABSA_glassdoor/dataset/employee_reviews.csv')
+    df = pd.read_csv(raw_file_path)
     new_df = pd.DataFrame()
     netflix_df = pd.DataFrame()
     stop_words = set(stopwords.words('english'))
@@ -33,17 +33,19 @@ def clean_review(raw_file_path, save_file_path):
             new_df.loc[index, 'senior-mangemnet-stars'] = int(float(row['senior-mangemnet-stars']))
 
             categories = ["summary", "pros", "cons", "advice-to-mgmt"]
-            text = ''
+            raw_text = ''
+            cleaned_text = ''
             context = ""
             for category in categories:
                 if type(row[category]) == str and row[category] != 'none':
                     context += row[category] + " "
-            text += ' '.join(
+            raw_text += context
+            cleaned_text += ' '.join(
                 [i.lower() for i in RegexpTokenizer(r'\w+').tokenize(context) if not i.lower() in stop_words])
 
-            new_df.loc[index, 'comments'] = text
-
-    new_df.to_csv('employee_reviews_cleaned.csv', index=False)
+            new_df.loc[index, 'raw_comments'] = raw_text
+            # new_df.loc[index, 'cleaned_comments'] = cleaned_text
+    new_df.to_csv(save_file_path, index=False)
 
 
 def get_lat_long(src_file_path, tgt_file_path):
@@ -67,27 +69,45 @@ def get_lat_long(src_file_path, tgt_file_path):
             geodata['lng'] = result['geometry']['location']['lng']
             geodata['address'] = result['formatted_address']
 
-            print('{address} (lat, lng) = ({lat}, {lng})'.format(**geodata))
-            loc_df.loc[idx, 'location'] = '{address}'.format(**geodata)
+            print('{} (lat, lng) = ({lat}, {lng})'.format(loc, **geodata))
+            loc_df.loc[idx, 'location'] = loc
             loc_df.loc[idx, 'lat'] = '{lat}'.format(**geodata)
             loc_df.loc[idx, 'lng'] = '{lng}'.format(**geodata)
     loc_df.to_csv(tgt_file_path, index=False)
 
-# if __name__ == '__main__':
-# clean_review(raw_file_path='/home/karen/workspace/ABSA_glassdoor/dataset/employee_reviews.csv',
-#              save_file_path='employee_reviews_cleaned.csv')
-#
-# df = pd.read_csv('/home/karen/workspace/ABSA_glassdoor/ABSA_LSTM_pt_update/employee_reviews_cleaned.csv')
-# df[df['company'] == 'google'].to_csv('google.csv', index=False)
-# df[df['company'] == 'amazon'].to_csv('amazon.csv', index=False)
-# df[df['company'] == 'microsoft'].to_csv('microsoft.csv', index=False)
-# df[df['company'] == 'facebook'].to_csv('facebook.csv', index=False)
-# df[df['company'] == 'apple'].to_csv('apple.csv', index=False)
-# df[df['company'] == 'netflix'].to_csv('netflix.csv', index=False)
-#
-# get_lat_long(src_file_path='locations.csv',
-#              tgt_file_path='locations_lat_lng.csv')
-# update_lat_lng(src_file_path='/opt/lampp/htdocs/DS330/final/dataset/employee_review_cleaned.csv',
-#                lat_lng_file_path='/home/karen/workspace/ABSA_glassdoor/ABSA_LSTM_pt_update/locations_lat_lng.csv',
-#                tgt_file_path='/opt/lampp/htdocs/DS330/final/dataset/employee_review_updated.csv')
+
+def update_lat_lng(src_file_path, lat_lng_file_path, tgt_file_path):
+    df = pd.read_csv(src_file_path)
+    loc_df = pd.read_csv(lat_lng_file_path)
+
+    loc_dict = loc_df.set_index('location').T.to_dict('list')
+
+    for idx, row in df.iterrows():
+        address = row['location']
+        try:
+            df.loc[idx, 'latitude'] = loc_dict[address][0]
+            df.loc[idx, 'longtitude'] = loc_dict[address][1]
+        except Exception as e:
+            print(address)
+    print(df)
+    df.to_csv(tgt_file_path, index=False)
+
+
+if __name__ == '__main__':
+    clean_review(raw_file_path='/home/karen/workspace/ABSA_glassdoor/dataset/employee_reviews.csv',
+                 save_file_path='/home/karen/workspace/ABSA_glassdoor/ABSA_LSTM_pt_update/employee_reviews_cleaned.csv')
+
+    # df = pd.read_csv('/opt/lampp/htdocs/DS330/final/dataset/employee_review_updated.csv')
+    # df[df['company'] == 'google'].to_csv('google.csv', index=False)
+    # df[df['company'] == 'amazon'].to_csv('amazon.csv', index=False)
+    # df[df['company'] == 'microsoft'].to_csv('microsoft.csv', index=False)
+    # df[df['company'] == 'facebook'].to_csv('facebook.csv', index=False)
+    # df[df['company'] == 'apple'].to_csv('apple.csv', index=False)
+    # df[df['company'] == 'netflix'].to_csv('netflix.csv', index=False)
+
+    # get_lat_long(src_file_path='locations.csv',
+    #              tgt_file_path='locations_lat_lng.csv')
+    update_lat_lng(src_file_path='/home/karen/workspace/ABSA_glassdoor/ABSA_LSTM_pt_update/employee_reviews_cleaned.csv',
+                   lat_lng_file_path='/home/karen/workspace/ABSA_glassdoor/ABSA_LSTM_pt_update/locations_lat_lng.csv',
+                   tgt_file_path='/opt/lampp/htdocs/DS330/final/dataset/employee_review_raw_comments.csv')
 
